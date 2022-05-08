53db6935c262af483d3ca9cfdc1507a27e7aa364
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-08 22:02:58 +0300

    created show, new crete methods and authentication

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,5 +1,30 @@
 class SurveysController < ApplicationController
+  before_action :authenticate_admin!, expept: [:index, :show]
+
   def index
     @surveys = Survey.all
+  end
+  
+  def show
+    @survey = Survey.find(params[:id])
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create 
+    @survey = Survey.new(survey_params)
+
+    if @survey.save
+      redirect_to @survey
+    else
+      render :new, status: :unprocessble_entity
+    end
+  end
+
+  private
+  def survey_params
+    params.require(:survey).permit(:title, :description)
   end
 end
