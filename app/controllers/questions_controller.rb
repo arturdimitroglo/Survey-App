9eebf0759abd9c8fa19fa7b28e3f444e74354190
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-11 18:41:57 +0300

    added methods to questions controler

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,29 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_admin!, except: %i[index show]
+
+  def new
+    @survey = Survey.find(params[:survey_id])
+    @question = @survey.questions.new
+    if params[:question_type] != nil
+      redirect_to return_url(params[:question_type], params[:survey_id]) 
+    end    
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question.survey
+    else
+      render :new, status: :unprocessble_entity
+    end
+  end
+
+  private
+  def return_url(question_type, survey_id)
+    url_from("surveys/#{survey_id}/questions/new_#{question_type}")
+  end
+  def question_params
+    params.require(:question).permit(:title, :question_type)
+  end
 end
