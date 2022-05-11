1edf67dba4e27d5e9e750bb3febcd3e84f5f5702
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-11 14:48:48 +0300

    created questions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_11_095059) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_11_100202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_11_095059) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.bigint "survey_id", null: false
+    t.string "question_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["survey_id"], name: "index_questions_on_survey_id"
+  end
+
   create_table "surveys", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -35,5 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_11_095059) do
     t.index ["admin_id"], name: "index_surveys_on_admin_id"
   end
 
+  add_foreign_key "questions", "surveys"
   add_foreign_key "surveys", "admins"
 end
