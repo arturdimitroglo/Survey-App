d6630a5786f7e1ab04bc551e1e901e50a4c5d8d7
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-12 11:38:38 +0300

    moved choices and open_ended answer from survey table to answer table

--- db/migrate/20220507151502_create_surveys.rb
+++ db/migrate/20220507151502_create_surveys.rb
@@ -3,13 +3,6 @@ class CreateSurveys < ActiveRecord::Migration[7.0]
     create_table :surveys do |t|
       t.string :title
       t.text :description
-      t.text :choice_1
-      t.text :choice_2
-      t.text :choice_3
-      t.text :choice_4
-      t.text :choice_5
-      t.text :choice_6
-      t.text :open_ended_answer
       t.timestamps
     end
   end
--- db/migrate/20220511100202_create_questions.rb
+++ db/migrate/20220511100202_create_questions.rb
@@ -4,7 +4,13 @@ class CreateQuestions < ActiveRecord::Migration[7.0]
       t.string :title
       t.references :survey, null: false, foreign_key: true
       t.string :question_type
-
+      t.text :choice_1
+      t.text :choice_2
+      t.text :choice_3
+      t.text :choice_4
+      t.text :choice_5
+      t.text :choice_6
+      t.text :open_ended_answer
       t.timestamps
     end
   end
