e3be806350e1c50bebdcd706a669769004da0383
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-15 12:57:54 +0300

    added added destroy method

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -37,6 +37,13 @@ class SurveysController < ApplicationController
       render :edit, status: :unprocessble_entity
     end
   end
+  
+  def destroy
+    @survey = Survey.find(params[:id])
+
+    @survey.destroy
+    redirect_to root_path, status: :see_other
+  end
 
   private
   def survey_params
