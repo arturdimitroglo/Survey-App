a7130c2ebe69d5848a72a242eb6284e31d44ae6c
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-15 20:33:37 +0300

    refactored answer forms

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,10 @@
 class AnswersController < ApplicationController
+  def create
+    
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:choice_1, :choice_2, :choice_3, :choice_4, :choice_5, :choice_6, :open_ended_answer)
+  end
 end
