98ebf15c9007f06744aa9b3a557d3228bb3cbce4
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-15 18:50:52 +0300

    added next and previous methods to questions model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,11 @@
 class Question < ApplicationRecord
   belongs_to :survey
+  
+   def next
+    Question.where("id > ?", id).first
+   end
+
+   def previous
+    Question.where("id < ?", id).last
+   end
 end
