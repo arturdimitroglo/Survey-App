ce992bfe7c810f5f1fbdd2ec34345a13953ba887
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-16 08:01:23 +0300

    created create method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,14 @@
 class AnswersController < ApplicationController
   def create
-    
+    @answer = Answer.new(answer_params)
+    if answer.save
+      redirect_to @answer.question
+    else
+    end
   end
 
   private
   def answer_params
-    params.require(:answer).permit(:choice_1, :choice_2, :choice_3, :choice_4, :choice_5, :choice_6, :open_ended_answer)
+    params.require(:answer).permit:(question_id,:choice_1, :choice_2, :choice_3, :choice_4, :choice_5, :choice_6, :open_ended_answer)
   end
 end
