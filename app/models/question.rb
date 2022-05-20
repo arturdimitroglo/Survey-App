8e9a33fc5dd508e4a3d099abdd915d570eb333b7
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-20 16:13:38 +0300

    added validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :survey
   has_many :answers, dependent: :destroy
+  validates :title, presence: true
   
    def next
     Question.where("id > ?", id).first
