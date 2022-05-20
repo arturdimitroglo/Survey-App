2e26d8c747c7e00c328d6cf0c0ff21b144d85680
Artur Dimitroglo <arturdimitrogl@gmail.com>
2022-05-20 16:12:50 +0300

    added validations

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,7 @@
 class Survey < ApplicationRecord
   belongs_to :admin
   has_many :questions, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
